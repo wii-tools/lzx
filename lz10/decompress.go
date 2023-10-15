a22af598bf96e6bbf47b16c0a734535c1dfa2de5
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2023-10-14 21:52:51 -0400

    Fix decompression

--- lz10/decompress.go
+++ lz10/decompress.go
@@ -22,36 +22,48 @@ func Decompress(data []byte) ([]byte, error) {
 
 	// Obtain the length of the decompressed file.
 	// We then drop the highest byte to strip the 0x10 magic.
-	header := append(compressed.Next(3), []byte{0}...)
+	fileSize := compressed.Next(3)
+	header := make([]byte, 4)
+	copy(header[:], fileSize[:])
 	originalLen := binary.LittleEndian.Uint32(header)
 
 	decompressed := new(bytes.Buffer)
-	var mask byte
-	var flags byte
-	var pos int
-	var length int
 
 	for decompressed.Len() < int(originalLen) {
-		mask >>= BitShiftCount
-		if mask == 0 {
-			flags = compressed.Next(1)[0]
-			mask = DefaultMask
-		}
+		b := compressed.Next(1)[0]
+		_bits := bits(b)
+		for _, bit := range _bits {
+			if bit == 1 {
+				_val := compressed.Next(2)
+				val := binary.BigEndian.Uint16(_val)
+				count := (val >> 0xC) + 3
+				disp := (val & 0xFFF) + 1
+				for i := 0; i < int(count); i++ {
+					length := decompressed.Len()
+					decompressed.WriteByte(decompressed.Bytes()[length-int(disp)])
+				}
+			} else {
+				decompressed.WriteByte(compressed.Next(1)[0])
+			}
 
-		if (flags & mask) == 0 {
-			decompressed.WriteByte(compressed.Next(1)[0])
-		} else {
-			pos = int(compressed.Next(1)[0])
-			pos = (pos << 8) | int(compressed.Next(1)[0])
-			length = (pos >> 12) + Threshold + 1
-			pos = (pos & 0xFFF) + 1
-
-			for length != 0 {
-				decompressed.WriteByte(decompressed.Bytes()[decompressed.Len()-pos])
-				length--
+			if int(originalLen) <= decompressed.Len() {
+				break
 			}
 		}
 	}
 
 	return decompressed.Bytes(), nil
+}
+
+func bits(passed byte) [8]byte {
+	return [8]byte{
+		(passed >> 7) & 1,
+		(passed >> 6) & 1,
+		(passed >> 5) & 1,
+		(passed >> 4) & 1,
+		(passed >> 3) & 1,
+		(passed >> 2) & 1,
+		(passed >> 1) & 1,
+		passed & 1,
+	}
 }
